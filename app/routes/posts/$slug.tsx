a51004ff82a1e5a654620d91de04c2b4a69be16d
fred <nobisan@gmail.com>
2022-06-23 16:58:42 -0700

    markdonw

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -3,21 +3,32 @@ import { useLoaderData, Link } from "@remix-run/react";
 
 import type { LoaderFunction } from "@remix-run/node";
 import { getPost } from "~/models/post.server";
+import type { Post } from "~/models/post.server";
+
+import { marked } from "marked";
+
+type LoaderData = { post: Post; html: string };
 
 export const loader: LoaderFunction = async ({ params }) => {
   const post = await getPost(params.slug);
-  return json({ post });
+
+  const html = marked(post.markdown);
+
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData();
+  const { post, html } = useLoaderData<LoaderData>();
 
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
         {post.title}
       </h1>
-      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{post.markdown}</div>
+      <div
+        className="mx-auto max-w-7xl sm:px-6 lg:px-8"
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
     </main>
   );
 }
